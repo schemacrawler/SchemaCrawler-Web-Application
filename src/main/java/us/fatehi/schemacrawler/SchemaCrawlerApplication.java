52f2d4b5ed0d3e66840da98fba27776d41104ebd
Sualeh Fatehi <sualeh@hotmail.com>
2017-01-29 15:21:52 -0500

    Initial commit

---
+++ src/main/java/us/fatehi/schemacrawler/SchemaCrawlerApplication.java
@@ -0,0 +1,12 @@
+package us.fatehi.schemacrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaCrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/us/fatehi/schemacrawler/SchemaCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package us.fatehi.schemacrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchemaCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
