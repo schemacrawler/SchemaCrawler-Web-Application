af3c8afedfaffabfa8f79b823be762b98d0d3936
Sualeh Fatehi <sualeh@hotmail.com>
2017-01-29 16:39:21 -0500

    Hello controller

--- src/main/java/us/fatehi/schemacrawler/SchemaCrawlerApplication.java
+++ src/main/java/us/fatehi/schemacrawler/SchemaCrawlerApplication.java
@@ -1,12 +1,16 @@
 package us.fatehi.schemacrawler;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchemaCrawlerApplication {
+public class SchemaCrawlerApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchemaCrawlerApplication.class, args);
-	}
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SchemaCrawlerApplication.class, args);
+  }
+  
 }
