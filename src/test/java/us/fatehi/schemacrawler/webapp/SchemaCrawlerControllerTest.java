d88de6ca933601770a4f813588af88bbf54816a2
Sualeh Fatehi <sualeh@hotmail.com>
2017-01-30 22:06:44 -0500

    Using Thymeleaf templates

---
+++ src/test/java/us/fatehi/schemacrawler/webapp/SchemaCrawlerControllerTest.java
@@ -0,0 +1,49 @@
+package us.fatehi.schemacrawler.webapp;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SchemaCrawlerControllerTest
+{
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void homePage()
+    throws Exception
+  {
+    mockMvc.perform(get("/index.html"))
+      .andExpect(content().string(containsString("Get your greeting")));
+  }
+
+  @Test
+  public void greeting()
+    throws Exception
+  {
+    mockMvc.perform(get("/greeting"))
+      .andExpect(content().string(containsString("Hello, World!")));
+  }
+
+  @Test
+  public void greetingWithUser()
+    throws Exception
+  {
+    mockMvc.perform(get("/greeting").param("name", "Greg"))
+      .andExpect(content().string(containsString("Hello, Greg!")));
+  }
+
+}
