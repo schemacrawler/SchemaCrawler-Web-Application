53097be3ccb648f9b432e95a74c5f046e6e10e3a
Sualeh Fatehi <sualeh@hotmail.com>
2017-02-08 20:32:57 -0500

    Use SQLite utility

--- src/main/java/us/fatehi/schemacrawler/webapp/schemacrawler/SchemaCrawlerSQLiteService.java
+++ src/main/java/us/fatehi/schemacrawler/webapp/schemacrawler/SchemaCrawlerSQLiteService.java
@@ -28,21 +28,9 @@ http://www.gnu.org/licenses/
 package us.fatehi.schemacrawler.webapp.schemacrawler;
 
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
 
 import org.springframework.stereotype.Service;
-
-import schemacrawler.schemacrawler.Config;
-import schemacrawler.schemacrawler.ConnectionOptions;
-import schemacrawler.schemacrawler.ExcludeAll;
-import schemacrawler.schemacrawler.SchemaCrawlerOptions;
-import schemacrawler.schemacrawler.SchemaInfoLevelBuilder;
-import schemacrawler.tools.executable.Executable;
-import schemacrawler.tools.integration.graph.GraphExecutable;
-import schemacrawler.tools.options.OutputOptions;
-import schemacrawler.tools.sqlite.SQLiteDatabaseConnector;
 
 @Service
 public class SchemaCrawlerSQLiteService
@@ -54,42 +42,7 @@ public class SchemaCrawlerSQLiteService
                                          final String extension)
     throws Exception
   {
-    try (final Connection connection = createDatabaseConnection(dbFile);)
-    {
-      return createSchemaCrawlerDiagram(connection, extension);
-    }
-  }
-
-  private Connection createDatabaseConnection(final Path file)
-    throws Exception
-  {
-    final Config config = new Config();
-    config.put("server", "sqlite");
-    config.put("database", file.toString());
-    final ConnectionOptions connectionOptions = new SQLiteDatabaseConnector()
-      .newDatabaseConnectionOptions(config);
-    return connectionOptions.getConnection();
-  }
-
-  private Path createSchemaCrawlerDiagram(final Connection connection,
-                                          final String extension)
-    throws Exception
-  {
-    final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
-    options.setSchemaInfoLevel(SchemaInfoLevelBuilder.standard());
-    options.setRoutineInclusionRule(new ExcludeAll());
-
-    final Path outputFile = Files.createTempFile("schemacrawler",
-                                                 "." + extension);
-    final OutputOptions outputOptions = new OutputOptions(extension,
-                                                          outputFile);
-
-    final Executable executable = new GraphExecutable("schema");
-    executable.setSchemaCrawlerOptions(options);
-    executable.setOutputOptions(outputOptions);
-    executable.execute(connection);
-
-    return outputFile;
+      return createSchemaCrawlerDiagram(dbFile, extension);
   }
 
 }
