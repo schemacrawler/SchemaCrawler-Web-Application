995837d64c44cd49240ede0e1b0f54ccdbe1568c
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-28 18:08:16 -0400

    Redirect to error page

--- src/main/java/us/fatehi/schemacrawler/webapp/controller/SchemaCrawlerDiagramController.java
+++ src/main/java/us/fatehi/schemacrawler/webapp/controller/SchemaCrawlerDiagramController.java
@@ -45,7 +45,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -57,7 +56,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import us.fatehi.schemacrawler.webapp.model.SchemaCrawlerDiagramRequest;
@@ -77,7 +75,6 @@ public class SchemaCrawlerDiagramController
   private SchemaCrawlerService scService;
 
   @ExceptionHandler(Throwable.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public String exception(final Throwable throwable, final Model model)
   {
     logger.error(throwable.getMessage(), throwable);
