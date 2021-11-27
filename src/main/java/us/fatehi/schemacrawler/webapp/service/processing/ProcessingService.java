6b2dbe2fe81341bb193f46a9a3908d61b6552fcb
Sualeh Fatehi <sualeh@hotmail.com>
2021-11-26 23:12:50 -0500

    Remove redundant interface

--- src/main/java/us/fatehi/schemacrawler/webapp/service/processing/ProcessingService.java
+++ src/main/java/us/fatehi/schemacrawler/webapp/service/processing/ProcessingService.java
@@ -27,22 +27,72 @@ http://www.gnu.org/licenses/
 */
 package us.fatehi.schemacrawler.webapp.service.processing;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.IOUtils.toInputStream;
+import static us.fatehi.schemacrawler.webapp.service.storage.FileExtensionType.JSON;
+import static us.fatehi.schemacrawler.webapp.service.storage.FileExtensionType.PNG;
+import static us.fatehi.schemacrawler.webapp.service.storage.FileExtensionType.SQLITE_DB;
+
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import us.fatehi.schemacrawler.webapp.model.DiagramKey;
 import us.fatehi.schemacrawler.webapp.model.DiagramRequest;
+import us.fatehi.schemacrawler.webapp.service.schemacrawler.SchemaCrawlerService;
+import us.fatehi.schemacrawler.webapp.service.storage.StorageService;
 
-/**
- * Service for processing.
- *
- * @author Sualeh Fatehi
- */
-public interface ProcessingService {
+@Service
+public class ProcessingService {
 
-  /**
-   * Generate SchemaCrawler diagram, and store it in storage
-   *
-   * @throws Exception On an exception
-   */
-  void generateSchemaCrawlerDiagram(
-      final DiagramRequest diagramRequest, final Path localPath) throws Exception;
+  private static final Logger logger =
+      Logger.getLogger(ProcessingService.class.getName());
+
+  private final StorageService storageService;
+  private final SchemaCrawlerService scService;
+
+  @Autowired
+  public ProcessingService(
+      @NotNull final StorageService storageService, @NotNull final SchemaCrawlerService scService) {
+    this.storageService = storageService;
+    this.scService = scService;
+  }
+
+  @Async
+  public void generateSchemaCrawlerDiagram(
+      @NotNull final DiagramRequest diagramRequest, @NotNull final Path localPath)
+      throws Exception {
+
+    logger.info(
+        String.format(
+            "Processing in thread %s%n%s", Thread.currentThread().getName(), diagramRequest));
+
+    final DiagramKey key = diagramRequest.getKey();
+
+    try {
+      // Store the uploaded database file
+      storageService.store(new PathResource(localPath), key, SQLITE_DB);
+
+      final String title = diagramRequest.getTitle();
+      // Generate a database integration, and store the generated image
+      final Path schemaCrawlerDiagram =
+          scService.createSchemaCrawlerDiagram(localPath, title, PNG.getExtension());
+      storageService.store(new PathResource(schemaCrawlerDiagram), key, PNG);
+
+    } catch (final Exception e) {
+      diagramRequest.setException(e);
+      throw e;
+    } finally {
+      // Store the JSON request
+      storageService.store(
+          new InputStreamResource(toInputStream(diagramRequest.toJson(), UTF_8)), key, JSON);
+    }
+  }
 }
